2a3cfcdde00dde53de18db7577ae1a8af07c6ee1
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-02-12 11:23:40 +0500

    Template embedding

---
+++ EcommerceWebApp/EcommerceWebApp/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
