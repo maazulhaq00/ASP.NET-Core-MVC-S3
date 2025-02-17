8b93c2aa96e8f123ea2091775c77d3733655f020
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-02-17 13:05:33 +0500

    Admin Profile Update

--- EcommerceWebApp/EcommerceWebApp/Controllers/AdminController.cs
+++ EcommerceWebApp/EcommerceWebApp/Controllers/AdminController.cs
@@ -8,9 +8,11 @@ namespace EcommerceWebApp.Controllers
     public class AdminController : Controller
     {
         private readonly EcommContext db;
-        public AdminController(EcommContext db)
+        IWebHostEnvironment env;
+        public AdminController(EcommContext db, IWebHostEnvironment env)
         {
             this.db = db;
+            this.env = env;
         }
         public IActionResult Index()
         {
@@ -49,6 +51,70 @@ namespace EcommerceWebApp.Controllers
             HttpContext.Session.Remove("adminimage");
 
             return RedirectToAction("Login");
+        }
+        public IActionResult Profile()
+        {
+            if (HttpContext.Session.GetString("adminuserid") != null)
+            {
+                //Work
+
+                int id = int.Parse(HttpContext.Session.GetString("adminuserid"));
+
+                var admin = db.tbl_admin.Find(id);
+
+                AdminViewModel admin1 = new AdminViewModel
+                {
+                    admin_id = admin.admin_id,
+                    admin_name = admin.admin_name,
+                    admin_email = admin.admin_email,
+                    admin_password = admin.admin_password,
+                };
+
+                ViewBag.admin_image = admin.admin_image;
+                
+                return View(admin1);
+            }
+
+            return RedirectToAction("Login");
+        }
+        [HttpPost]
+        public IActionResult Profile(AdminViewModel admin1)
+        {
+            int id = int.Parse(HttpContext.Session.GetString("adminuserid"));
+
+            var admin = db.tbl_admin.Find(id);
+
+            if (ModelState.IsValid)
+            {
+
+
+                string fileName = admin.admin_image;
+
+                if(admin1.admin_image != null)
+                {
+                    string folderPath = Path.Combine(env.WebRootPath, "images","admin");
+                    fileName = Guid.NewGuid().ToString() +"_"+  admin1.admin_image.FileName;
+                    string fullPath = Path.Combine(folderPath, fileName);
+
+                    admin1.admin_image.CopyTo(new FileStream(folderPath, FileMode.Create));
+
+                }
+
+                admin.admin_name = admin1.admin_name;
+                admin.admin_email = admin1.admin_email;
+                admin.admin_password = admin1.admin_password;
+                admin.admin_image = fileName;
+               
+
+                db.tbl_admin.Update(admin);
+                db.SaveChanges();
+
+            }
+
+            ViewBag.admin_image = admin.admin_image;
+
+            return View();
+
         }
     }
 }
---
+++ EcommerceWebApp/EcommerceWebApp/Models/AdminViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWebApp.Models
+{
+    public class AdminViewModel
+    {
+        [Key]
+        public int admin_id { get; set; }
+        public string admin_name { get; set; }
+        public string admin_email { get; set; }
+        public string admin_password { get; set; }
+        public IFormFile? admin_image { get; set; }
+    }
+}
